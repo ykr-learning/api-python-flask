2bdb05fb3ce1f22d0a1f631d834ad91db6d48e84
ykr-learning <>
2022-06-14 17:08:24 +0200

    fix port

--- app.py
+++ app.py
@@ -1,3 +1,70 @@
-from src.app import start_app
+from flask import Flask, jsonify, render_template, request
 
-start_app()
+
+def create_app():
+    app = Flask(__name__)
+
+    stores = [
+        {
+            'name': 'My_wonderful_store',
+            'items': [
+                {
+                    'name': 'My Item',
+                    'price': 15.99
+                }
+            ]
+        }
+    ]
+
+    @app.route('/')
+    def home():
+        # render_template search the file in the folder templates
+        return render_template('index.html'), 200
+
+    @app.route('/store/', methods=['POST'])
+    def create_store():
+        request_data = request.get_json()
+        new_store = {
+            'name': request_data['name'],
+            'items': []
+        }
+        stores.append(new_store)
+        return jsonify(new_store), 201
+
+    @app.route('/store/<string:name>')
+    def get_store(name):
+        for store in stores:
+            if store['name'] == name:
+                return jsonify(store), 200
+        return jsonify({'message': 'store not found'}), 404
+
+    @app.route('/store')
+    def get_stores():
+        return jsonify({'stores': stores}), 200
+
+    @app.route('/store/<string:name>/item', methods=['POST'])
+    def create_item_in_store(name):
+        request_data = request.get_json()
+        for store in stores:
+            if store['name'] == name:
+                new_item = {
+                    'name': request_data['name'],
+                    'price': request_data['price']
+                }
+                store['items'].append(new_item)
+                return jsonify(new_item), 201
+        return jsonify({'message': 'store not found'}), 404
+
+    @app.route('/store/<string:name>/item')
+    def get_items_in_store(name):
+        for store in stores:
+            if store['name'] == name:
+                return jsonify({'items': store['items']}), 200
+        return jsonify({'message': 'store not found'}), 404
+
+    return app
+
+
+if __name__ == '__main__':
+    myapp = create_app()
+    myapp.run(port=8000)
